559295baf02426b468e20b4effe5323c33427039
HTmonster <Theo_hui@163.com>
2019-05-10 22:17:14 +0800

    done: 添加了LSTM_CNN模型https://github.com/pmsosa/CS291K/blob/master/lstm_cnn.py todo：1. 程序基于python2 需要进一步迁移       2. 需要编写train程序

---
+++ page_identify/LSTM_CNN.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+#   ____    ____    ______________
+#  |    |  |    |  |              |
+#  |    |  |    |  |_____    _____|
+#  |    |__|    |       |    |
+#  |     __     |       |    |
+#  |    |  |    |       |    |
+#  |    |  |    |       |    |
+#  |____|  |____|       |____|
+#
+# fileName:LSTM_CNN 
+# project: Fish_learning
+# author: theo_hui
+# e-mail:Theo_hui@163.com
+# purpose: LSTM_CNN模型
+#          LSTM_CNN模型兼具了RNN和CNN的优点，考虑了句子的序列信息，又能捕捉关键信息 但是 没法并行计算，因此，训练时速度要比FastText和TextCNN慢得多
+# creatData:2019/5/10
+import tensorflow as tf
+import numpy as np
+from IPython import embed
+
+
+class LSTM_CNN(object):
+    def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters,
+                 l2_reg_lambda=0.0, num_hidden=100):
+        # PLACEHOLDERS
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")  # X - The Data
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")  # Y - The Lables
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")  # Dropout
+
+        l2_loss = tf.constant(0.0)  # Keeping track of l2 regularization loss
+
+        # 1. EMBEDDING LAYER ################################################################
+        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            self.W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
+            # self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+
+        # 2. LSTM LAYER ######################################################################
+        self.lstm_cell = tf.contrib.rnn.LSTMCell(32, state_is_tuple=True)
+        # self.h_drop_exp = tf.expand_dims(self.h_drop,-1)
+        self.lstm_out, self.lstm_state = tf.nn.dynamic_rnn(self.lstm_cell, self.embedded_chars, dtype=tf.float32)
+        # embed()
+
+        self.lstm_out_expanded = tf.expand_dims(self.lstm_out, -1)
+
+        # 2. CONVOLUTION LAYER + MAXPOOLING LAYER (per filter) ###############################
+        pooled_outputs = []
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # CONVOLUTION LAYER
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                conv = tf.nn.conv2d(self.lstm_out_expanded, W, strides=[1, 1, 1, 1], padding="VALID", name="conv")
+                # NON-LINEARITY
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # MAXPOOLING
+                pooled = tf.nn.max_pool(h, ksize=[1, sequence_length - filter_size + 1, 1, 1], strides=[1, 1, 1, 1],
+                                        padding='VALID', name="pool")
+                pooled_outputs.append(pooled)
+
+        # COMBINING POOLED FEATURES
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(pooled_outputs, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+
+        # #3. DROPOUT LAYER ###################################################################
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total, num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+
+        print
+        "(!!) LOADED LSTM-CNN! :)"
+        # embed()
+
+# 1. Embed --> LSTM
+# 2. LSTM --> CNN
+# 3. CNN --> Pooling/Output
